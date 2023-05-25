77b785a06877e18168db7d70cb891975ab32a52b
Arnold <122205085+RNOL14@users.noreply.github.com>
2023-05-25 18:22:45 +0530

    Add files via upload

---
+++ js/ht.js
@@ -0,0 +1,11 @@
+
+    function myfunction() {
+
+        document.getElementById("khii").innerHTML="Fuck you bro";
+        document.getElementById("khii").style.color="black";
+        document.getElementById("khii").style.fontSize="100px";
+
+
+        
+    }
+
